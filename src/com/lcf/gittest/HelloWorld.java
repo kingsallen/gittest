d59051a8f54610acf7552d4014665e3912a3148d
licf <lcfei1988@126.com>
2018-07-07 20:40:30 +0800

    hello w

---
+++ src/com/lcf/gittest/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.lcf.gittest;
+
+import org.junit.Test;
+
+/**
+ * Created by wlee on 2018/7/7.
+ */
+public class HelloWorld {
+
+    @Test
+    public void get(){
+        System.out.println("get method");
+    }
+}
